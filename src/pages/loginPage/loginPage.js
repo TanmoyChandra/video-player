6c70c5c31ced029d7e4bff7fc9536cb27c2566a5
parnasha <parnasha.s@digitaledge360.in>
2024-01-28 11:24:41 +0530

    initial commit

---
+++ src/pages/loginPage/loginPage.js
@@ -0,0 +1,11 @@
+import "./loginPage.css";
+
+function LoginPage() {
+  return (
+    <>
+      <h1 className="login_heading">This is a Log in page..</h1>
+    </>
+  );
+}
+
+export default LoginPage;
