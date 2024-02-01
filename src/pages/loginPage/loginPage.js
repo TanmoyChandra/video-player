f4daa053ab06d1962a0ce4c647bdc7c1f2a7a32d
parnasha <parnasha.s@digitaledge360.in>
2024-02-01 22:15:25 +0530

    login page

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -3,7 +3,14 @@ import "./loginPage.css";
 function LoginPage() {
   return (
     <>
-      <h1 className="login_heading">This is a Log in page..</h1>
+    <div className="login_form">
+      <center>
+      <h2>Enter your mail id </h2>
+      <input type="text"></input>
+      <h2>Enter your password </h2>
+      <input type="password"></input>
+      </center>
+      </div>
     </>
   );
 }
